9417f948fa66ee10082436af4d2a89923f736f9c
andresrivasrod <andres.rivas@correo.unimet.edu.ve>
2024-03-20 16:01:46 -0400

    Projects descriptions corrected

--- public/assets/data.js
+++ public/assets/data.js
@@ -4,7 +4,7 @@ export const data = [
       id: 1, imgUrl: "assets/ProjectImages/Bingo Image.png", link: "https://andresrivasrod.github.io/microproyecto1/", name:"Bingo", description:"This personal project is a Bingo simulator buit in HTML, CSS and JavaScript."
   },
   {
-      id: 2, imgUrl: "assets/ProjectImages/Memory Game Image.png" , link: "https://andresrivasrod.github.io/memory-game/", name: "Memory Game", description:"This personal project is memory game uses images of WWE superstars and was built using HTML, CSS and JavaScript."
+      id: 2, imgUrl: "assets/ProjectImages/Memory Game Image.png" , link: "https://andresrivasrod.github.io/memory-game/", name: "Memory Game", description:"This personal project is a memory game that uses images of WWE superstars and was built using HTML, CSS and JavaScript."
   },
   {
       id: 3, imgUrl: "assets/ProjectImages/Pomodoro Image.png" , link: "https://andresrivasrod.github.io/pomodoro-timer/", name: "Pomodoro Timer", description:"This personal project is a Pomodoro Timer that was built using HTML, CSS and Javascript."
@@ -13,6 +13,6 @@ export const data = [
       id: 4, imgUrl: "assets/ProjectImages/Weather App.png" , link: "https://andresrivasrod.github.io/weather-app/", name: "Weather App", description:"This is a personal project app that was made in HTML, CSS and Javascript using the OpenWeather API."
   },
   {
-    id: 5, imgUrl: "assets/ProjectImages/Transporte SABAS.png" , link: "https://transporte-sabas.web.app/", name: "Transporte SABAS Bastidas, C.A. Web Page", description:"This web page was built using React, hosted in Firebase. It was made for de company Transporte SABAS Bastidas, C.A."
+    id: 5, imgUrl: "assets/ProjectImages/Transporte SABAS.png" , link: "https://transporte-sabas.web.app/", name: "Transporte SABAS Bastidas, C.A. Web Page", description:"This web page was built using React and hosted in Firebase. It was made for the company: Transporte SABAS Bastidas, C.A."
   }
 ];
